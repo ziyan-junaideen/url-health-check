0052f8d24c30ba1ea609257daacd6268dab8e94c
Ziyan Junaideen <jdeen-solutions@outlook.com>
2013-12-13 12:06:24 +0530

    Init

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,15 @@
+class SiteController < ApplicationController
+
+  def index 
+
+  end
+
+  def contact
+
+  end
+
+  def check 
+
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UrlCheck::Application.config.secret_key_base = 'b0f4457fa2b4d0fb4b3560ab6d93e3107fecd88e672f97dd8fd57ad415b83ca630f404fc0020776478cd4c05931b9d706eb4c41d5a53d654354b65abd55496e0'
