823603837e8e14da6040a5dfe02ff3ab7192abc3
Ziyan Junaideen <jdeen-solutions@outlook.com>
2013-12-13 12:24:53 +0530

    URL helper

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,2 +1,31 @@
 module SiteHelper
+
+  def split_urls s
+    parts = s.split(',')
+    urls = []
+    parts.each do | part |
+      urls << part.strip
+    end
+    urls
+  end
+
+  def check_url u 
+    
+    begin 
+      
+      require 'net/http'
+      url = URI.parse(u)
+      req = Net::HTTP.new(url.host, url.port)
+      res = request_head(url.path)
+
+      return true if res.code == '200'
+
+    rescue Exception => e 
+      return false
+    end
+
+    return false
+    
+  end
+
 end
